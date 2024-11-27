070e0e94fab0b8111c4588fb2b936f93b95af290
Dani Harmade <12250310356@students.uin-suska.ac.id>
2024-11-27 17:03:54 +0700

    [Feature] Message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.googleid)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
